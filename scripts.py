b99547ea742b5d885fd13963dacf20b22030cff4
Kevin Fu <kevinzefu@gmail.com>
2021-12-03 12:02:02 -0600

    massage the cards

--- scripts.py
+++ scripts.py
@@ -1,28 +1,103 @@
 import json
 
 
-def tmp(c, name, effect):
+def give_effect(c, name, effect):
     if c['type'] == name:
         if c.get('effect') is None:
-            c['effect'] = effect
-            c['target'] = []
-            c['direction'] = ['self']
+            e = {}
+            e['effect'] = effect
+            e['resources'] = c['resources']
+            e['target'] = []
+            e['direction'] = ['self']
+            c['effects'] = [e]
+            del (c['resources'])
 
 
-with open("cards.json") as f:
-    cards = json.load(f)
+def give_effects():
+    with open("cards2.json") as f:
+        cards = json.load(f)
 
 
-for card in cards:
-    tmp(card, 'common', 'produce')
-    tmp(card, 'luxury', 'produce')
-    tmp(card, 'military', 'strength')
-    tmp(card, 'civilian', 'victory')
-    # tmp(card, 'commercial') ignore commercial b/c wacky-doodle
-    tmp(card, 'science', 'produce')
+    for card in cards:
+        give_effect(card, 'common', 'produce')
+        give_effect(card, 'luxury', 'produce')
+        give_effect(card, 'military', 'strength')
+        give_effect(card, 'civilian', 'victory')
+        # tmp(card, 'commercial') ignore commercial b/c wacky-doodle
+        give_effect(card, 'science', 'produce')
 
-with open('cards.json', 'w') as f:
-    json.dump(cards, f, indent=2)
+    with open('cards3.json', 'w') as f:
+        json.dump(cards, f, indent=2)
 
 
+def tmp(c):
+    if c.get('effect') is not None:
+        c_effect = c.get('effect')
+        effects = []
+        for i, effect in enumerate(c_effect):
+            d = {}
+            d["effect"] = c_effect[i]
+            if c['name'] == "Scientists Guild":
+                d["resources"] = c['resources']
+            elif len(c['resources']) > 0:
+                d["resources"] = [c['resources'][i]]
+            else:
+                d["resources"] = []
+            d["target"] = c['target']
+            d["direction"] = c['direction']
+            effects.append(d)
+        c['effects'] = effects
 
+        del(c['target'])
+        del(c['effect'])
+        del(c['direction'])
+        del(c['resources'])
+
+
+def effect_to_effects():
+    with open("cards.json") as f:
+        cards = json.load(f)
+
+    for card in cards:
+        tmp(card)
+
+    with open('cards2.json', 'w') as f:
+        json.dump(cards, f, indent=2)
+
+
+def modify(c):
+    if c.get('effects') is None:
+        print(c)
+        return
+
+    for e in c['effects']:
+        if e['effect'] == "produce" or e['effect'] == "discount":
+            continue
+
+        if e['effect'] == "levy":
+            e['resources'] = ["c" * e['resources'][0]]
+            e['effect'] = "generate"
+
+        if e['effect'] == "strength":
+            e['resources'] = ["m" * e['resources'][0]]
+            e['effect'] = "generate"
+
+        if e['effect'] == "victory":
+            e['resources'] = ["v" * e['resources'][0]]
+        print(e)
+
+
+def modify_effects():
+    with open("cards3.json") as f:
+        cards = json.load(f)
+
+    for card in cards:
+        modify(card)
+
+    with open('cards4.json', 'w') as f:
+        json.dump(cards, f, indent=2)
+
+
+effect_to_effects()
+give_effects()
+modify_effects()
