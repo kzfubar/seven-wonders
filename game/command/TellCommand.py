1d91bdae8ad93c1324c42aadc08b2391a715a791
Kevin Fu <kevinzefu@gmail.com>
2023-03-05 19:44:48 -0600

    augment tell command

--- game/command/TellCommand.py
+++ game/command/TellCommand.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from game.command.GameCommand import GameCommand
 from networking.server.ClientConnection import ClientConnection
 
@@ -5,10 +7,11 @@ from networking.server.ClientConnection import ClientConnection
 class TellCommand(GameCommand):
     name: str = "tell"
 
-    def execute(self, args: str, client: ClientConnection):
-        player_name = args[0]
-        msg = args[1]
+    def execute(self, args: List, client: ClientConnection):
+        player_name = args.pop(0)
+        msg = ' '.join(args)
+        messenger = self.game.players_by_client[client]
         if player_name in self.game.players_by_name.keys():
-            self.game.players_by_name[player_name].display(msg)
+            self.game.players_by_name[player_name].display(f"{messenger}: {msg}")
         else:
             client.send_message(f"{player_name} not found. players: {self.game.players_by_name.keys()}")
