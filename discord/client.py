40d1f9fd6cce42dced622695b311f3975d2c8f54
Sprite <ponkaew.wasin@gmail.com>
2023-03-23 22:05:07 -0500

    test slash commands

--- discord/client.py
+++ discord/client.py
@@ -1,13 +1,16 @@
 import discord
+from discord import app_commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    await tree.sync(guild=discord.Object(id=681633844151189554))
 
 @client.event
 async def on_message(message):
@@ -17,7 +20,11 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-with open('token.txt') as f:
-    token = f.read()
+@tree.command(name="test", description="test", guild=discord.Object(id=681633844151189554))
+async def tester(msg):
+    await msg.response.send_message("slash successful")
+
+with open("discord/token.txt", "r") as r:
+    token = r.read()
 
 client.run(token)
